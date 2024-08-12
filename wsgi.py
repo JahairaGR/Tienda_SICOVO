004b829694327db8e4348970e3aca7a9910c6f15
JahairaGR <20213tn063@utez.edu.mx>
2024-08-12 04:39:47 +0000

    Subiendo la app LIBROLANDIA a GitHub

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import app
-
-# Cargar configuración desde un archivo específico si es necesario
-# app.config.from_object('config.Config')  # Ajusta esta línea si necesitas cargar configuraciones específicas
-
-if __name__ == "__main__":
-    app.run()
