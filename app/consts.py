6619fb0fa98d004faebdca51ad57ec91a71fc9b3
JahairaGR <105512694+JahairaGR@users.noreply.github.com>
2024-08-15 02:33:52 -0600

    Delete app directory

--- app/consts.py
+++
@@ -1,5 +0,0 @@
-LOGIN_CREDENCIALESINVALIDAS='¡Usuario o Password Incorrectos!'
-LOGOUT='Cerraste Sesión Exitosamente'
-
-MENSAJE_BIENVENIDA='¡Bienvenid@ a la Librería Librolandia!'
-MENSAJE_REGISTRADO='!Registrado exitosamente¡'
